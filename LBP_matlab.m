a724a81503cf696df17fba5903e31239160d40df
quimicobr <davi.nl@hotmail.com>
2017-03-13 00:46:05 +0100

    Update LBP_matlab.m
    Caracteristicas adicionadas:
    
     - No inicio, nao apaga mais a celula images, que contem todas as imagens, visto que a leitura das imagens nas suas pastas demora
     - Os plots estao todos comentados, para otimizar o codigo
     - Foi adicionado o calculo do chi carre para os histogramas do LBP hollistic e spatially enhanced, para divisoes de 3x3, 5x5 e 7x7
     - O calculo do chi carre é feito para diversas imagens, o que permite calcular a media do chicarre entre as imagens de uma mesma pasta (em relaçao à imagem inicial de indices N_dossier e N_image)
    
    Defeitos ainda existentes:
    
     - Codigo so funciona se N_images é igual à 10. Tem que fazer uma boucle for que passe pela imagem original mas nao a leia
     - Calcular a média e mostrar de uma forma melhor para o LBP spatially enhanced 
     - Calcular o desvio padrao do chi carre
    
    Ainda falta:
    
     - Entender e aplicar a parte 5 do BE

--- LBP_matlab.m
+++ LBP_matlab.m
@@ -1,104 +1,154 @@
-clear all
+clearvars -except images
 close all
 
-for n_paste = 1:2; %Choisit le dossier d'o� prendre les images
+ex = exist('images');
+if (ex == 0)
+    for n_paste = 1:40; %Choisit le dossier d'où prendre les images
 
-d = dir(['ORL/S', num2str(n_paste), '/*.bmp']); %Ouvre le dossier
-nfiles = length(d); %Compte le nombre d'images
+        d = dir(['ORL/S', num2str(n_paste), '/*.bmp']); %Ouvre le dossier
+        nfiles = length(d); %Compte le nombre d'images
 
-for i = 1:nfiles
-    images{n_paste,i} = imread(['ORL/S', num2str(n_paste) ,'/',num2str(i),'.bmp']);
-end
+        for i = 1:nfiles
+            images{n_paste,i} = imread(['ORL/S', num2str(n_paste) ,'/',num2str(i),'.bmp']);
+        end
+    end
 end
 %Stocke toutes les images du dossier sur la cellule images
+%Pourtant, on ne l'efface pas au début. Donc, on le lit seulement si c'est
+%la première fois qu'on exécute le code
 
-im = images{1,1};
-%On prend seulement la premi�re image du dossier
+N_dossier = 1;
+N_image = 10;
+
+im = images{N_dossier,N_image};
+%On prend d'abord l'image au dossier numéro N_dossier, et dans le dossier,
+%l'image numéro N_image
 
 LBP_im = LBP(im); % Fonction qui donne le LBP de l'image
 
 [h, l] = size(im); %h et l dimensions de l'image
 
-edges = 0:256;
+edges = 0:256; %Vecteur qui donne les limites de chaque barre de l'histogramme
 
-figure;
-subplot(1,3,1);
-imshow(im);
+% figure;
+% subplot(1,3,1);
+% imshow(im);
+% 
+% subplot(1,3,2);
+% imshow(LBP_im);
+% 
+% subplot(1,3,3);
 
-subplot(1,3,2);
-imshow(LBP_im);
+hist1_entier = histcounts(LBP_im, edges);
+%Vecteur histogramme de l'image LBP entière 
 
-subplot(1,3,3);
-hist1 = histogram(LBP_im, edges);
-axis([0 256 0 max(hist1.Values)]);
+% bar(hist1);
+% axis([0 256 0 max(hist1)]);
 %Montre l'image original, l'image LBP et son histogramme
 
-%Maintenant, il faut faire le LBP 'spatially enhanced', c'est-�-dire pour
-%pour r�gions 3x3, 5x5 et 7x7
-
-%Il faut faire une fonction qui re�oit l'image en LBP et envoie � la sortie
-%la variable histogramme de chacune des r�gions
+%Maintenant, il faut faire le LBP 'spatially enhanced', c'est-à-dire pour
+%pour régions 3x3, 5x5 et 7x7
 
 n = 0;
-div_dim = [3 5 7];
+%Initialise n, qui donnera les positions dans la cellule des histogrammes
+%des images LBP decoupées
+div_dim = [3 5 7]; 
+%Vecteur qui indique les trois options de découpe de l'image LBP 
 
 for k = div_dim
 
     n = n + 1;
-    [hist{n}, maxi(n)] = decoupe(LBP_im, k);
+    [hist1_dec{n}, maxi(n)] = decoupe(LBP_im, k);
+    %La cellule hist1_dec contient tous les vecteurs histogrammes des
+    %images LBM découpées
+    %maxi contient les valeurs maximales pour chaque tableau d'histogrammes
+    
+    
 
-    figure;
-    for i = 1:k
-        for j = 1:k
-            subplot(k,k,(i-1)*k+j);
-            bar(hist{n}{i,j});
-            axis([0 256 0 maxi(n)]);
-        end
-    end
+%     figure;
+%     for i = 1:k
+%         for j = 1:k
+%             subplot(k,k,(i-1)*k+j);
+%             bar(hist{n}{i,j});
+%             axis([0 256 0 maxi(n)]);
+%         end
+%     end
             
 end
 
-
-h_LBP = h - 2;
+h_LBP = h - 2; 
 l_LBP = l - 2;
+%La hauteur et la largeur de l'image LBP sont 2 pixels plus petites que les
+%originales, puisqu'on prend seulement les pixels de centre
 
-%Maintenant, il faut faire une distribution de chi carr� entre le LBP d'une
-%image e d'autre. D'abord, pour le LBP d'une image enti�re, puis divis� par
-%r�gions, avec diff�rents poids pour chaque r�gion
-                   
-im2 = images{1,2}; %Lit une deuxi�me image
+%Maintenant, il faut faire une distribution de chi carré entre le LBP d'une
+%image e d'autre. D'abord, pour le LBP d'une image entière, puis divisé par
+%régions, avec différents poids pour chaque région
 
-LBP_im2 = LBP(im2); %Fait le LBP de la deuxi�me image
 
-chi2 = 0; %Initialise la valeur du chi carr�
+for nd = 1:10
+    %Cette boucle en nd fait varier le dossier pour lequel on prend la 
+    %deuxième image 
+for ni = 1:9
+    %Cette boucle en ni fait varier le numéro d'image dans un dossier pour
+    %lequel on prend la deuxième image    
 
-%%
-%for i = 1:h_LBP
-%    for j = 1:l_LBP
-%        chi2 = chi2 + (LBP_im(i,j) - LBP_im2(i,j)).^2./(LBP_im(i,j) + LBP_im2(i,j));
-%        %Calcule le chi carr�
-%    end
-%end
-%%%%
-%Calcule probablement mauvais, il faut parcourir l'histogramme, pas l'image
-%%
+im2 = images{nd,ni}; %Lit une deuxième image
 
-hist2 = histcounts(LBP_im2, edges);
+LBP_im2 = LBP(im2); %Fait le LBP de la deuxième image
 
-for i = 1:256
-    if (hist1.Values(i) ~= hist2(i))
-        chi2 = chi2 + (hist1.Values(i) - hist2(i))^2/(hist1.Values(i) + hist2(i));
+hist2_entier = histcounts(LBP_im2, edges);%Histogramme du LBP de la deuxième image entière
+
+% figure;
+% subplot(1,3,1);
+% imshow(im2);
+% 
+% subplot(1,3,2);
+% imshow(LBP_im2);
+% 
+% subplot(1,3,3);
+% bar(hist2);
+% axis([0 256 0 max(hist2)]);
+
+chi2_entier(ni,nd) = chicarre(hist1_entier,hist2_entier);
+%Calcule le chi2 entre la première et la deuxième image
+
+%Maintenant, il faut calculer le chi carré pour la division par régions
+n = 0;
+
+for k = div_dim
+    n = n+1;
+
+hist2_dec = decoupe(LBP_im2, k);
+%On prend les histogrammes de chaque région
+
+clearvars mat_chi2
+for i = 1:k
+    for j = 1:k        
+        mat_chi2(i,j) = chicarre(hist1_dec{n}{i,j},hist2_dec{i,j})
+        %Calcule le chi carre pour chaque petite région de l'image et le
+        %stocke sur une matrice. Cette matrice sera nécéssaire après pour
+        %appliquer les coefficients de poids
     end
 end
 
-%Maintenant, il faut calculer le chi carr� pour la division par r�gions
-%D'abord, on prend les r�gions de la nouvelle image
+mat = matpoids(k);
+%On calcule les poids pour chaque région dans le calcule du coefficient de
+%similarité, en utilisant la fonction matpoids
 
-hist2_dec = decoupe(LBP_im2, 7);
+chi2_dec(ni,nd,n) = sum(sum(mat.*mat_chi2));
+%Finalement, on calcule le chi carre pour chaque image (par rapport à la
+%première image choisie), aussi bien que pour chaque nombre de divisions
+%(3x3, 5x5 et 7x7)
 
+end
+end
+end
 
+moy_dec = sum(chi2_dec)/9;
 
+moy_entier = sum(chi2_entier)/9;
 
-
-                    
-
+%On calcule la moyenne des chi carre entre toutes les images d'un même
+%dossier, pour les images découpées (spacially enhanced) ou non-découpées
+%(hollistic)
